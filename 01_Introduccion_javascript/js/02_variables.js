f1c8067c91ae237d311e775534850da5d50c62f4
Sergio Gomez <sergio.gmzdl@gmail.com>
2022-06-02 11:24:52 +0200

    Variables

---
+++ 01_Introduccion_javascript/js/02_variables.js
@@ -0,0 +1,26 @@
+/* --------------------------------
+            VARIABLES 
+---------------------------------*/
+
+//Numericas
+var numero = 1;
+console.log('numero:', numero);
+
+//String
+var nombre = 'Luis';
+console.log('nombre:', nombre);
+
+//Booleana
+var booleana = true;
+console.log('booleana:', booleana);
+
+//Array
+var colores = ['rojo', 'amarillo']
+console.log('color:', colores[0]);
+
+//Objeto
+var zumo = {ingrediente1: 'platano',
+            ingrediente2: 'fresa',
+            ingrediente3: 'manzana'}
+console.log('zumo', zumo);
+console.log('ingrediente:', zumo.ingrediente2);
