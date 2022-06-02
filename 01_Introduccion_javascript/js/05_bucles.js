4a7c0a5e317256b4abc7e5221a6bc0340762cf00
Sergio Gomez <sergio.gmzdl@gmail.com>
2022-06-02 13:42:39 +0200

    Manipular DOM con bucle for

--- 01_Introduccion_javascript/js/05_bucles.js
+++ 01_Introduccion_javascript/js/05_bucles.js
@@ -7,10 +7,24 @@ for(var i = 0; i <= 5; i++){
     console.log('i:', i);
 }
 
+//Caso de ejemplo para manipular el DOM con for
+var cajas = document.querySelectorAll('.cajas');
+console.log(cajas);
+
+for(var i = 0; cajas.length; i++){
+
+	cajas[i].style.width = '50px';
+	cajas[i].style.height = '50px';
+	cajas[i].style.background = 'blue';
+    cajas[i].style.marginTop = '5px';
+	cajas[i].style.marginRight = '5px';//CSS margin-left, en JavaScript no puedes incluir guiones
+	cajas[i].style.display = 'inline-block';
+}
+
 //While
 var n = 0;
 
-while(n<=5){
+while(n <= 5){
     console.log(n);
     n++;    
 }
@@ -21,7 +35,7 @@ do{
     console.log('n:', n);
     n++;
 }
-while(n<=5);
+while(n <= 5); 
 
 
 
