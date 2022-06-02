bd27585cca3e03a06ad29c7e67adc4a120429db5
Sergio Gomez <sergio.gmzdl@gmail.com>
2022-06-02 13:17:45 +0200

    Bucles(for, while & do_while)

---
+++ 01_Introduccion_javascript/js/05_bucles.js
@@ -0,0 +1,27 @@
+/* --------------------------------
+            BUCLES 
+---------------------------------*/
+
+//For
+for(var i = 0; i <= 5; i++){
+    console.log('i:', i);
+}
+
+//While
+var n = 0;
+
+while(n<=5){
+    console.log(n);
+    n++;    
+}
+
+//Do while
+var n = 0;
+do{
+    console.log('n:', n);
+    n++;
+}
+while(n<=5);
+
+
+
