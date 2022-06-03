c35ec45c5ccaf980ba36a71eb6127e1883ec6d68
Sergio Gomez <sergio.gmzdl@gmail.com>
2022-06-03 13:35:58 +0200

    POO_clases

---
+++ 02_POO/js/01_clases.js
@@ -0,0 +1,26 @@
+/*-------------------------------------------------------------------- 
+            CLASES CREADAS POR EL DESARROLADOR
+            (Otras clases vienen ya predefinidas, por ejemplo: 
+            String, Number, Boolean) 
+---------------------------------------------------------------------*/
+
+function Persona(){
+    this.nombre;
+    this.edad;
+}
+
+var persona = new Persona();
+persona.nombre = "Luis";
+persona.edad = 30;
+
+console.log("Persona: ", persona);
+
+
+//Clases con parametros
+function Animales(nombre, raza){
+    this.nombre = nombre;
+    this.raza = raza;
+}
+
+var mascota = new Animales("perro","Pitbull");
+console.log("Mascota: ", mascota);
