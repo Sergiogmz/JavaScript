170cb6499a37fdc89cc4b224b80e92cc8bcee380
Sergio Gomez <sergio.gmzdl@gmail.com>
2022-06-03 11:56:09 +0200

    Evento en JavaScript

--- 01_Introduccion_javascript/js/06_eventos.js
+++ 01_Introduccion_javascript/js/06_eventos.js
@@ -1,5 +1,21 @@
+/* --------------------------------
+            EVENTOS EN EL DOM 
+---------------------------------*/
+
 var cuadrado = document.querySelector("#cuadrado");
 
 function cambiarColor(){
 	cuadrado.style.background = "orange";
+}
+
+/* --------------------------------
+            EVENTOS EN JAVASCRIPT 
+---------------------------------*/
+
+var boton = document.querySelector("#boton");
+boton.addEventListener("click", moverCaja);
+
+function moverCaja(){
+	cuadrado.style.width = "300px";
+	cuadrado.style.transition = "1s width ease";
 }
