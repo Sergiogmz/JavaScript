362e691c53abde9c8a26be65e434491ed4049d16
Sergio Gomez <sergio.gmzdl@gmail.com>
2022-06-03 12:30:59 +0200

    Intervalos

---
+++ 01_Introduccion_javascript/js/07_Intervalos.js
@@ -0,0 +1,25 @@
+/* -------------------------------------------------
+            SET INTERVAL(intervalo de tiempo)
+            setInterval(funcion, tiempo)
+---------------------------------------------------*/
+var tiempo = document.querySelector("#tiempo");
+var segundos = 0;
+
+var intervalo = setInterval(function(){
+
+    segundos++;
+    tiempo.innerHTML = segundos;
+    //tiempo.innerHTML += segundos;
+
+}, 1000);
+
+/* -------------------------------------------------
+            SET TIMEOUT(retardo de tiempo)
+            setTimeout(funcion, tiempo)
+---------------------------------------------------*/
+
+setTimeout(function(){
+
+    clearInterval(intervalo);
+
+}, 5000);
